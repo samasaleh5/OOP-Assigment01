040b67b4c7b13d5d74101aebb4bafbab58bed7de
samasaleh5 <ss2104@fayoum.edu.eg>
2024-12-23 21:23:45 +0200

    question2 solved

---
+++ OOP-Assigment01/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assigment01
+{
+    internal struct Person
+    {
+        public string Name { get; set; }
+        public int age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            this.age = age;
+        }  
+    }
+}
--- OOP-Assigment01/Program.cs
+++ OOP-Assigment01/Program.cs
@@ -10,9 +10,21 @@
         static void Main(string[] args)
         {
             #region question1
-            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(day);
+            //}
+            #endregion
+
+            #region question2
+            Person[] persons = new Person[3];
+            persons[0] = new Person { Name = "Sama", age = 23 };
+            persons[1] = new Person { Name = "Zeyad", age = 24 };
+            persons[2] = new Person { Name = "Habiba", age = 24 };
+
+            foreach (Person person in persons) 
             {
-                Console.WriteLine(day);
+                Console.WriteLine($"Name:{person.Name} , Age:{person.age}");
             }
             #endregion
 
