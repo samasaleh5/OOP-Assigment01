d6164b6bd2c42afe1cbaff63bef755df681bde9a
samasaleh5 <ss2104@fayoum.edu.eg>
2024-12-23 21:17:12 +0200

    question1 solved

--- OOP-Assigment01/Program.cs
+++ OOP-Assigment01/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
+        enum WeekDays
+        {
+            monday, tuesday, wednesday, thursday, friday, saturday, sunday
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region question1
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
+            #endregion
+
+
         }
     }
 }
